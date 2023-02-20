324d4525e79cc2a6e1616758532522b1673c8c94
William D <william@evolt360.com>
2023-02-20 12:20:23 +1000

    added region

--- applications/helloworld-openapi-gateway-lambda/backend.tf
+++ applications/helloworld-openapi-gateway-lambda/backend.tf
@@ -2,7 +2,7 @@ terraform {
     backend "s3" {
         encrypt              = true
         bucket               = "terraform-state-file"
-        region               = "ap-southeast-2"
+        region               = "ap-southeast-1"
         key                  = "helloworld-openapi-gateway.tfstate"
         profile              = "default"
         workspace_key_prefix = "wk7_terraform"
