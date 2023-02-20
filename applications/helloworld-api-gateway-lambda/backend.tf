0ca96def86a0b6cfd5214269d85d0cd4457eda5f
William D <william@evolt360.com>
2023-02-20 12:22:37 +1000

    fixed bucket region

--- applications/helloworld-api-gateway-lambda/backend.tf
+++ applications/helloworld-api-gateway-lambda/backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
         encrypt              = true
-        bucket               = "terraform-state-file"
+        bucket               = "evolt-failover-tfstate"
         region               = "ap-southeast-1"
         key                  = "helloworld-api-gateway.tfstate"
         profile              = "default"
