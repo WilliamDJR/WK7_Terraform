e56aea619b7517915f9480ae31dcaa2aad1777f9
William <william@13900K.com>
2023-02-20 19:29:02 +1000

    fixed provider

--- applications/helloworld-api-gateway-lambda/backend.tf
+++ applications/helloworld-api-gateway-lambda/backend.tf
@@ -1,4 +1,13 @@
 terraform {
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 4.16"
+        }
+    }
+
+    required_version = ">= 1.2.0"    
+
     backend "s3" {
         encrypt              = true
         bucket               = "tfstate-willido"
