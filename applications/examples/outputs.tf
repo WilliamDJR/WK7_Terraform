8deaad15fb5641ad9e113fe702639daf20997450
William D <william@evolt360.com>
2023-02-21 15:56:18 +1000

    added ubuntu name

--- applications/examples/outputs.tf
+++ applications/examples/outputs.tf
@@ -1,3 +1,7 @@
 output "ubuntu_images" {
-  value = data.aws_ami_ids.ubuntu.ids
+  value = data.aws_ami_ids.ubuntu_ids.ids
+}
+
+output "ubuntu_names" {
+  value = data.aws_ami.ubuntu.name
 }
--- applications/examples/ubuntu_data_resource.tf
+++ applications/examples/ubuntu_data_resource.tf
@@ -1,4 +1,4 @@
-data "aws_ami_ids" "ubuntu" {
+data "aws_ami_ids" "ubuntu_ids" {
   owners = ["099720109477"]
 
   filter {
@@ -6,7 +6,14 @@ data "aws_ami_ids" "ubuntu" {
     values = ["ubuntu/images/ubuntu-*-*-amd64-server-*"]
   }
 }
+data "aws_ami" "ubuntu" {
+    owners      = ["099720109477"] # Canonical
 
+    filter {
+        name   = "image-id"
+        values = data.aws_ami_ids.ubuntu_ids.ids[0] # replace with the actual AMI ID
+    }
+}
 # resource "aws_instance" "example" {
 #   ami           = data.aws_ami.ubuntu.id
 #   instance_type = "t2.micro"
