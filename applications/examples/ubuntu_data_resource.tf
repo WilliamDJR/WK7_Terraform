3029a878e5d9d4e252b4d35712217523d9a4b4e2
William D <william@evolt360.com>
2023-02-21 15:41:28 +1000

    fixed working directory

--- applications/examples/ubuntu_data_resource.tf
+++ applications/examples/ubuntu_data_resource.tf
@@ -3,7 +3,7 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-*-22.04-*-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-*-*-*-*"]
   }
 
   filter {
