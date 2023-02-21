bad33519bcca96ffb66034a40c3dcd5d85a73109
William D <william@evolt360.com>
2023-02-21 11:00:48 +1000

    removed ttl of dynamodb

--- modules/dynamodb/dynamodb.tf
+++ modules/dynamodb/dynamodb.tf
@@ -16,11 +16,6 @@ resource "aws_dynamodb_table" "helloworld-dynamodb-table" {
     type = "S"
   }
 
-  ttl {
-    attribute_name = "TimeToExist"
-    enabled        = false
-  }
-
   global_secondary_index {
     name               = "HelloIndex"
     hash_key           = var.attributes[0]
