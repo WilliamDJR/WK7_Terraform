81e4e1bc21053c93c8a66d489db61bb0ad779db3
William D <william@evolt360.com>
2023-02-22 10:27:46 +1000

    added lookup

--- applications/examples/outputs.tf
+++ applications/examples/outputs.tf
@@ -2,9 +2,14 @@ output "ubuntu_images" {
   value = data.aws_ami_ids.ubuntu_ids.ids
 }
 
+locals {
+  ubuntu22 = lookup(data.aws_ami.ubuntu,"22.04")
+}
+
+
 output "ubuntu_names" {
   value = {
-    for u in data.aws_ami.ubuntu:
+    for u in ubuntu22:
       u.image_id => u.name
   }
 }
