b8ef0cecbba3cfd5b5424d4bf85b41e7025332c3
William D <william@evolt360.com>
2023-02-22 16:11:26 +1000

    added lookup

--- applications/examples/variables.tf
+++ applications/examples/variables.tf
@@ -1,8 +1,8 @@
 variable "ubuntu_instance_images" {
     type = map
     default = {
-            ubuntu18 = "ami-00070c60585f808dc"
+            ubuntu18 = "ami-0075a67de5ab4fa5e"
             ubuntu20 = "ami-00c76c78e78a3dcd4"
-            ubuntu22 = "ami-00eab542a0ac25a2d"
+            ubuntu22 = "ami-06933f99c7b8ac912"
     }
 }
