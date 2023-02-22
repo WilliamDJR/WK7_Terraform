0c7be0eeea1aef5d67e6b473011e1e0ead106934
William <william@13900K.com>
2023-02-22 18:28:38 +1000

    changed ubuntu ami id

--- applications/examples/variables.tf
+++ applications/examples/variables.tf
@@ -1,8 +1,8 @@
 variable "ubuntu_instance_images" {
     type = map
     default = {
-            ubuntu18 = "ami-0075a67de5ab4fa5e"
-            ubuntu20 = "ami-00c76c78e78a3dcd4"
-            ubuntu22 = "ami-06933f99c7b8ac912"
+            ubuntu18 = "ami-0331cab4f961a4e3e"
+            ubuntu20 = "ami-0ec19a300f3097b5a"
+            ubuntu22 = "ami-08f0bc76ca5236b20"
     }
 }
